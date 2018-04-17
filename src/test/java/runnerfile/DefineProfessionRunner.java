54865a4a125f67e435f66b1c07b23194f556efb9
fspl001147 <rohit.1102.rk@gmail.com>
2018-04-17 17:55:24 +0530

    17-04-2018

---
+++ src/test/java/runnerfile/DefineProfessionRunner.java
@@ -0,0 +1,19 @@
+package runnerfile;
+
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(ExtendedCucumber.class)
+@CucumberOptions(
+        features = {"featurefiles/DefineStaffType.feature"}
+        ,glue= {"stepdefinitions"}
+        ,monochrome=true
+        ,plugin= {"pretty","html:target/cucumber_html_report",
+        "json:target/cucumber.json",
+        "junit:target/cucumber.xml"}
+        ,tags= {"@Scenario1"}
+)
+public class DefineProfessionRunner {
+}
